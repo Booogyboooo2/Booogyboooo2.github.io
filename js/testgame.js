888c9838e13b678eced18d39cd909665896d1c84
Booogyboooo2 <spamsignup16@gmail.com>
2024-06-12 20:53:27 +0000

    fix maybe

--- js/testgame.js
+++ js/testgame.js
@@ -1,9 +1,9 @@
 function start() {
     var height = prompt("height: ");
-    var width = prompt("width: ")
-    var game = new Game(height, width)
+    var width = prompt("width: ");
+    var game = new Game()
     Replay.setStart(new Date().getTime());
-    game.load();
+    game.load(height, width);
 }
 
 function createAction(id, checked, time) {
@@ -16,17 +16,14 @@ function replay() {
 }
 
 class Game {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-    }
-
-    load() {
+    load(h, w) {
         var elem = document.getElementById("game");
         var i = 0;
         var e = 0;
-        while (i <= this.height) {
-            while (e <= this.width) {
+        console.log("height:" + this.height)
+        console.log("width:" + this.width)
+        while (i < h) {
+            while (e <= w) {
                 elem.innerHTML += "<input id='" + i + "_" + e + "' type='checkbox'>" ;
                 document.getElementById(i + "_" + e).addEventListener("click", createAction(i + "_" + e, document.getElementById(i + "_" + e).checked, new Date().getTime()));
                 e++;
@@ -41,32 +38,32 @@ class Game {
 
 class Replay {
     constructor() {
-        this.actions = [""];
-        this.start = null;
-        this.end = null;
-        this.len = null;
+        actions = [];
+        start = null;
+        end = null;
+        len = null;
     }
 
     static addAction(action) {
-        this.actions[this.actions.length - 1] = action;
+        actions[actions.length] = action;
     }
 
     static setStart(time) {
-        this.start = time;
+        start = time;
     }
 
     static setEnd(time) {
-        this.end = time;
+        end = time;
     }
 
     static startReplay() {
-        this.len = this.end - this.start;
-        while (this.len > (this.end - new Date().getTime())) {
+        len = end - start;
+        while (len > (end - new Date().getTime())) {
             var i = 0;
-            while (i < this.action.length) {
-                if (this.action[i].time <= (this.end - new Date().getTime()) && this.action[i].ran == false) {
-                    document.getElementById(this.action[i].id).checked = this.action[i].checked;
-                    this.action[i].ran = true;
+            while (i < action.length) {
+                if (action[i].time <= (end - new Date().getTime()) && action[i].ran == false) {
+                    document.getElementById(action[i].id).checked = action[i].checked;
+                    action[i].ran = true;
                 }
                 i++;
             }
