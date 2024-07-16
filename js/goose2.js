0f725cb7b4e924fe6407bd37e5448c2290842321
Booogyboooo2 <spamsignup16@gmail.com>
2024-07-16 19:46:21 +0000

    temp fix

--- js/goose2.js
+++ js/goose2.js
@@ -482,7 +482,7 @@ class MethodModif {
 
     static injectMethod(method, code) {
         try {
-            var prev = GooseV2.prototype[method].toString().replaceAll("\n", "").replaceAll(" ", "").replace(method + "(){", "").slice(0, -1).replaceAll("elseif", "else if").replaceAll(")", ").bind(g)");
+            var prev = GooseV2.prototype[method].toString().replaceAll("\n", "").replaceAll(" ", "").replace(method + "(){", "").slice(0, -1).replaceAll("elseif", "else if").replaceAll("()", "().bind(g)");
             console.log(prev.toString());
             eval("GooseV2.prototype." + method + " = function () { " + prev + code + "}");
             console.log("Set code too: GooseV2.prototype." + method + " = function () { " + prev + code + "}");
